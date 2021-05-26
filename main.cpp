48309e5bcc95690e8655163bff74b4205f0f8e0a
e_t <77915530+e-t-sudo@users.noreply.github.com>
2021-05-26 21:05:57 +0530

    Cube::solve() function calls from main()

--- main.cpp
+++ main.cpp
@@ -1,14 +1,21 @@
 #include "./headers/Cube.hpp"
 #include "./headers/Edges.hpp"
 #include "./headers/Corners.hpp"
-
+void Cube::solve(std::string edgeConfigFile, std::string cornerConfigFile){
+	Corners::solve(cornerConfigFile, Cube::solvedCornerFile);
+	Edges::solve(edgeConfigFile, Cube::solvedEdgeFile);
+}
 int main(int argc, char * argv[]){
-    Corners cornerLattice("./test_cubes/corners.txt", "./test_cubes/solvedCorners.txt");
-    cornerLattice.orient();
-    cornerLattice.printCornerOrientMoves();
-    cornerLattice.permute();
-    cornerLattice.printCornerPermuteMoves();
-    Edges edgeLattice("./test_cubes/edges.txt", "./test_cubes/solvedEdges.txt");
-    edgeLattice.permute();
-    edgeLattice.printPermuteMoves();
+	//+--------------------------------------------------------------+
+	//| Cube::solve(EDGE_CONFIG_FILE_PATH, CORNER_CONFIG_FILE_PATH); |
+	//+--------------------------------------------------------------+
+	std::cout << "Solution for test #1\n";
+	Cube::solve("./tests/test01/edges.txt", "./tests/test01/corners.txt");
+	std::cout << "Solution for test #2\n";
+	Cube::solve("./tests/test02/edges.txt", "./tests/test02/corners.txt");
+	std::cout << "Solution for test #3\n";
+	Cube::solve("./tests/test03/edges.txt", "./tests/test03/corners.txt");
+	std::cout << "Solution for test #4\n";
+	Cube::solve("./tests/test04/edges.txt", "./tests/test04/corners.txt");
+return 0;
 }
